9249bcc2a9247f5855dadbc5cef8aaebbef777ae
riuboh <rint1925@gmail.com>
2026-04-12 10:05:51 +0900

    change: cronjob

--- bin/cron-job.sh
+++ bin/cron-job.sh
@@ -2,4 +2,4 @@
 export PATH="/home/user/.bun/bin:$PATH"
 export HOME=/home/user
 
-/home/user/.local/bin/claude -p "discordのチャンネル:1492397421274337320に「テスト」と送信して"
+/home/user/.local/bin/claude -p "PRTIMESのRSS(https://prtimes.jp/index.rdf)を見て「農業・人材育成」の話題を抽出し,discordのチャンネル:1492397421274337320に内容を送信して"
